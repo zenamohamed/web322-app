c68f80d5c56d0113123cd4c70b3788064a9586a3
Larry Okuonghae <elokuonghae@myseneca.ca>
2024-05-15 20:47:25 -0400

    updated package.json

--- server.js
+++ server.js
@@ -1,16 +1,2 @@
 /**
-*  WEB322 - Assignment 1
-*  I declare that this assignment is my own work in accordance with Seneca Academic Policy. 
-*  No part of this assignment has been copied manually or electronically from any other source
-*  (including web sites, friends gpt or otherwise) or distributed to other students.
-*  I understand that if caught doing so, I will receive zero on this assignment and possibly 
-*  fail the entire course.
-*  Name: Larry Okuonghae
-*  Student ID: 145203238
-*  Date: 15-05-2024
-*  Vercel Web App URL: https://vercel.com/larry-okuonghaes-projects/web322-app
-*  GitHub Repository URL: https://github.com/Elo07/web322-app.git
-**/
-
-
-console.log("Larry Okuonghae - 145203238")
+*  WEB322 - Assignment
